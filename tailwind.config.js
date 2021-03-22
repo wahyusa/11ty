147cd12187e8a41596945dfcfc32b5a107c55518
Wahyu Syamsul A'lam <wahyuskull22@gmail.com>
2021-03-22 12:46:13 +0700

    Ahh real init

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: {
+    layers: ['components', 'utilities'],
+    content: ['*.html'],
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
